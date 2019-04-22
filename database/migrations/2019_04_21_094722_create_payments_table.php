eadd53fff73e327605881d3d2153e92155f9da40
kubo <kubokouta@sm-tap.com>
2019-04-22 20:50:47 +0900

    データベースの作成

---
+++ database/migrations/2019_04_21_094722_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('order_no');
+            $table->unsignedInteger('price');
+            $table->string('customer');
+            $table->date('issue_date')->nullable();
+            $table->string('note');
+            $table->string('method');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('shop_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
